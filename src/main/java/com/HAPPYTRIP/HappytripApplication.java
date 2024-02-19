a16836efa379bc0d125dfa593e9beff4a296ceca
sujeong <crystal17312@naver.com>
2024-02-20 01:46:33 +0900

    feature-A

--- src/main/java/com/HAPPYTRIP/HappytripApplication.java
+++ src/main/java/com/HAPPYTRIP/HappytripApplication.java
@@ -8,6 +8,8 @@ public class HappytripApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HappytripApplication.class, args);
+
+		System.out.println("진원 뚱바보");
 	}
 
 }

a7c913705492e0a4f65e35f4ad0f65ac05894d12
ajw711 <ajw711@naver.com>
2024-02-20 01:58:50 +0900

    feature-B

--- src/main/java/com/HAPPYTRIP/HappytripApplication.java
+++ src/main/java/com/HAPPYTRIP/HappytripApplication.java
@@ -6,11 +6,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HappytripApplication {
 
+	static class A{
+
+
+
+
+
+		void print(){
+			int a=1;
+			int b=2;
+			System.out.println(a+b);
+		}
+
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HappytripApplication.class, args);
 
+		A a=new A();
+		a.print();
+
 
 		System.out.println("수정이 바보");
+
 	}
 
 

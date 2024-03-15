e2cca99f60b1ba16a6bc708ddd045b31cbfc87f1
sujeong <crystal17312@naver.com>
2024-03-16 01:46:17 +0900

    관리자 회원관리

--- src/main/java/com/HAPPYTRIP/controller/AdminController.java
+++ src/main/java/com/HAPPYTRIP/controller/AdminController.java
@@ -1,18 +1,42 @@
 package com.HAPPYTRIP.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
+import com.HAPPYTRIP.domain.Member;
+import com.HAPPYTRIP.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
 
+    private final MemberRepository memberRepository;
 
-    @GetMapping("/main")
-    public String admin() {
-        return "admin";
+    @GetMapping("/members")
+    public String adminMembers(Model model) {
+        List<Member> memberList = this.memberRepository.findAll();
+        model.addAttribute("memberList", memberList);
+        return "adminMembers";
+    }
+
+    @GetMapping("/booking")
+    public String adminBooking() {
+        return "adminBooking";
+    }
+
+    @GetMapping("/notice")
+    public String adminNotice() {
+        return "adminNotice";
+    }
+
+    @GetMapping("/board")
+    public String adminBoard() {
+        return "adminBoard";
     }
 
 }
